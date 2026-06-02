b6bdb33a2f187036f096b4c27acb51b99998c87c
stackfiendllc-debug <stackfiendllc@gmail.com>
2026-06-01 19:27:14 -0700

    Update supabase.js

--- src/js/supabase.js
+++ src/js/supabase.js
@@ -4,7 +4,4 @@ const supabaseUrl = "https://thmgouvftmknpdhnfzpo.supabase.co";
 
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRobWdvdXZmdG1rbnBkaG5menBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMzc1NDksImV4cCI6MjA5NTkxMzU0OX0.DVb-2s5hRPeHsXRF5OC9ypl93CkFhnvSJd-Yx-HSVyQ";
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-);
+export const supabase = createClient(supabaseUrl, supabaseKey);
